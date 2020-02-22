46fd1ce87585e20ea3d29390f6c2a4779b3b51ce
Aman Chermashev <bakytaman228337@gmail.com>
2020-02-22 19:12:25 +0600

    [BACKEND] Created initial structure in index

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 
+const news = require('./app/News');
+
 const app = express();
 const port = 8001;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Server started on ${port} port!`);
-});
+app.use('/', news);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
